bcedba753fc6b7da30046e8a590a2a7474401d72
Robin Varghese <robinvk6@gmail.com>
2019-11-17 22:05:33 -0500

    Fix OAuth Token.

--- communis-peripherals/src/main/java/com/rhema/communis/core/swagger/SwaggerConfig.java
+++ communis-peripherals/src/main/java/com/rhema/communis/core/swagger/SwaggerConfig.java
@@ -1,55 +1,43 @@
 package com.rhema.communis.core.swagger;
 
+import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.schema.ModelRef;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.Contact;
-import springfox.documentation.service.Parameter;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.SecurityConfiguration;
+import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
-    private List<Parameter> listDocketParameters;
-
-    public SwaggerConfig() {
-
-        //Any parameter or header you want to require for all end_points
-        Parameter oAuthHeader = new ParameterBuilder()
-                .name("X-Auth-Token")
-                .description("JWT Token")
-                .defaultValue("YourJWTTokenHere")
-                .modelRef(new ModelRef("string"))
-                .parameterType("header")
-                .required(true)
-                .build();
-
-        this.listDocketParameters = new ArrayList<Parameter>();
-        this.listDocketParameters.add(oAuthHeader);
-    }
-
-
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .globalOperationParameters(listDocketParameters)
+                //.globalOperationParameters(listDocketParameters)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.rhema"))
                 .paths(PathSelectors.any())
                 .build()
+                .securitySchemes(Arrays.asList(apiKey()))
+                .securityContexts(Arrays.asList(securityContext()))
                 .apiInfo(apiInfo());
+    }
+
+    public SecurityConfiguration securityConfiguration() {
+        return SecurityConfigurationBuilder.builder()
+                .appName("Communis")
+                .build();
     }
 
     private ApiInfo apiInfo() {
@@ -63,6 +51,21 @@ public class SwaggerConfig {
     }
 
     private ApiKey apiKey() {
-        return new ApiKey("authkey", "X-Auth-Token", "header");
+        return new ApiKey("apiKey", "X-Auth-Token", "header");
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("apiKey", authorizationScopes));
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(Predicates.and(PathSelectors.regex("/api.*"),
+                        Predicates.not(Predicates.equalTo("auth")))::apply)
+                .build();
     }
 }
