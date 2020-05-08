d2842925e272fd7a357b01b46ef31bbfc232648e
Jerin Mathew <>
2020-05-08 01:54:09 -0400

    Allow updating address of person separately

--- communis-services/src/main/java/com/rhema/communis/person/PersonService.java
+++ communis-services/src/main/java/com/rhema/communis/person/PersonService.java
@@ -1,12 +1,15 @@
 package com.rhema.communis.person;
 
 import com.rhema.communis.common.AbstractService;
+import com.rhema.communis.domain.Address;
 import com.rhema.communis.mission.domain.person.PersonDerived;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PersonService extends AbstractService<PersonDerived, String> {
@@ -18,6 +21,13 @@ public class PersonService extends AbstractService<PersonDerived, String> {
     @Autowired
     public PersonService(MongoRepository<PersonDerived, String> personRepository){
         this.personRepository = personRepository;
+    }
+
+    public PersonDerived updateAddress(String personId, List<Address> addresses) {
+        PersonDerived person = this.find(personId);
+        person.setAddress(addresses);
+        this.saveOrUpdate(person);
+        return this.find(personId);
     }
 
 }
