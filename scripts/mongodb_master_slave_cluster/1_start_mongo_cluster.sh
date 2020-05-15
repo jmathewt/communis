8b49090662be022fd3df9dc532261df300d566c9
Jerin Mathew <>
2020-05-15 12:09:05 -0400

    Add master slave cluster configuration scripts

---
+++ scripts/mongodb_master_slave_cluster/1_start_mongo_cluster.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+. ../utils.sh
+
+print_info "Starting docker containers"
+docker-compose up -d
+
+print_info "Sleeping 3 seconds before initiating replica sets"
+sleep 3
+
+print_info "Initiate replica sets"
+echo 'rs.initiate({
+    _id : "replica_set1",
+    members: [
+        {_id:0, host:"mdb_replica_set1_node1:27017"},
+        {_id:1, host:"mdb_replica_set1_node2:27017"},
+        {_id:2, host:"mdb_replica_set1_node3:27017"},
+    ]
+})' | docker-compose exec -T mdb_replica_set1_node1 mongo
+
+print_info "Sleeping 3 seconds before connecting replica sets"
+sleep 3
+
+print_info "Connect mongo replica sets"
+docker-compose exec mdb_replica_set1_node3 mongo --host replica_set1/mdb_replica_set1_node3,mdb_replica_set1_node3,mdb_replica_set1_node3
+
+. ./cluster_status.sh
+
