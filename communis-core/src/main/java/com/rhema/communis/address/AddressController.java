936bfc5b43a60f0fbc9f2d94598fff6f7e6ab2b8
Jobin Thomas <jmathewt@gmail.com>
2020-06-16 00:35:06 -0400

    extend AbstractController where ever possible

--- communis-core/src/main/java/com/rhema/communis/address/AddressController.java
+++ communis-core/src/main/java/com/rhema/communis/address/AddressController.java
@@ -1,12 +1,9 @@
 package com.rhema.communis.address;
 
 import com.rhema.communis.common.AbstractController;
-import com.rhema.communis.common.CommunisError;
 import com.rhema.communis.common.CommunisResponse;
 import com.rhema.communis.domain.Address;
 import io.swagger.annotations.Api;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,28 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/address")
+@RequestMapping("/api/core/address")
 @Api(tags = "Core Apis")
-public class AddressController {
-
-    private final AddressService addressService;
-
-    @Autowired
-    public AddressController(AddressService addressService){
-        this.addressService = addressService;
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<CommunisResponse> find (@PathVariable String id){
-        Address address = addressService.find(id);
-        if(address == null){
-            return new ResponseEntity<CommunisResponse>(new CommunisResponse(
-                    new CommunisError("Unable to find address with id - " + id)),
-                    HttpStatus.NOT_FOUND);
-        }
-
-        return new ResponseEntity<CommunisResponse>(new CommunisResponse(address),
-                HttpStatus.OK);
-    }
+public class AddressController extends AbstractController<Address, String> {
 
 }
